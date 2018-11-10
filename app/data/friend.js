229924f59848394c6b2878fe45a32fa197535356
JMP <porcayojm@gmail.com>
2018-11-10 13:01:27 -0800

    adding app

---
+++ app/data/friend.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+        name: "The Hulk",
+        
+        scores: [
+            5,
+            4,
+            4,
+            1,
+            2
+        ]
+
+    },
+    {
+        name: "Thor",
+       
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Black Widow",
+        
+        scores: [
+            2,
+            1,
+            5,
+            4,
+            3
+        ]
+
+    }
+];
+
+module.exports = friends;
+
